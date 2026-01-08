72661fb68470496c03af58c6d40d4b298d92359d
JesseOgunlaja <jesseogunlaja@gmail.com>
2026-01-08 18:48:38 +0000

    fix: make changes needed for streamthing v7

--- app/(auth)/magic/[code]/route.ts
+++ app/(auth)/magic/[code]/route.ts
@@ -1,62 +1,65 @@
-import { eq } from "drizzle-orm";
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
 import { createWebsocketStream } from "@/server/actions/lib";
 import { setAccessToken, setRefreshToken, signJWT } from "@/server/auth";
 import { db, kv } from "@/server/db/db";
 import { sessions, users } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 interface ParamsType {
-	params: Promise<{
-		code: string;
-	}>;
+    params: Promise<{
+        code: string;
+    }>;
 }
 
 export async function GET(request: NextRequest, { params }: ParamsType) {
-	try {
-		const code = (await params).code;
+    try {
+        const code = (await params).code;
 
-		const email = (await kv.get(`magic:${code}`)) as string | null;
-		if (!email) {
-			return NextResponse.json({ message: "Invalid code" }, { status: 401 });
-		}
+        const email = (await kv.get(`magic:${code}`)) as string | null;
+        if (!email) {
+            return NextResponse.json(
+                { message: "Invalid code" },
+                { status: 401 }
+            );
+        }
 
-		const existingUser = await db.query.users.findFirst({
-			where: eq(users.email, email),
-		});
+        const existingUser = await db.query.users.findFirst({
+            where: eq(users.email, email),
+        });
 
-		const { id } =
-			existingUser ||
-			(
-				await db
-					.insert(users)
-					.values({ email, name: email.split("@")[0] })
-					.returning({ id: users.id })
-			)[0];
+        const { id } =
+            existingUser ||
+            (
+                await db
+                    .insert(users)
+                    .values({ email, name: email.split("@")[0] })
+                    .returning({ id: users.id })
+            )[0];
 
-		const [{ sessionId }] = await db
-			.insert(sessions)
-			.values({ userId: id })
-			.returning({ sessionId: sessions.id });
-		await setRefreshToken(sessionId);
+        const [{ sessionId }] = await db
+            .insert(sessions)
+            .values({ userId: id })
+            .returning({ sessionId: sessions.id });
+        await setRefreshToken(sessionId);
 
-		const [access_token] = await Promise.all([
-			signJWT({ userId: id }),
-			kv.del(`magic:${code}`),
-		]);
+        const [access_token] = await Promise.all([
+            signJWT({ userId: id }),
+            kv.del(`magic:${code}`),
+        ]);
 
-		await setAccessToken(access_token);
+        await setAccessToken(access_token);
 
-		const stream = createWebsocketStream();
+        const stream = await createWebsocketStream();
 
-		const channel = (await cookies()).get("device_id")?.value as string;
-		stream.send(channel, "login", "");
+        const channel = (await cookies()).get("device_id")?.value as string;
+        stream.send(channel, "login", "");
 
-		return NextResponse.redirect(new URL("/dashboard", request.url));
-	} catch {
-		return NextResponse.json(
-			{ message: "An unexpected error occurred, please try again." },
-			{ status: 500 },
-		);
-	}
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    } catch {
+        return NextResponse.json(
+            { message: "An unexpected error occurred, please try again." },
+            { status: 500 }
+        );
+    }
 }
--- app/(protected)/invite/[code]/route.ts
+++ app/(protected)/invite/[code]/route.ts
@@ -1,50 +1,52 @@
-import { after, NextRequest, NextResponse } from "next/server";
 import { createWebsocketStream } from "@/server/actions/lib";
 import { db, kv } from "@/server/db/db";
 import { roles } from "@/server/db/schema";
 import { getUserFromEmail } from "@/server/lib";
+import { after, NextRequest, NextResponse } from "next/server";
 
 interface ParamsType {
-	params: Promise<{
-		code: string;
-	}>;
+    params: Promise<{
+        code: string;
+    }>;
 }
 
 export async function GET(request: NextRequest, { params }: ParamsType) {
-	const code = decodeURIComponent((await params).code);
-	const [email, projectId] = String(await kv.get(`invite:${code}`)).split(":");
+    const code = decodeURIComponent((await params).code);
+    const [email, projectId] = String(await kv.get(`invite:${code}`)).split(
+        ":"
+    );
 
-	const user = await getUserFromEmail(email);
+    const user = await getUserFromEmail(email);
 
-	if (!user)
-		return NextResponse.json({ message: "Invalid code" }, { status: 401 });
+    if (!user)
+        return NextResponse.json({ message: "Invalid code" }, { status: 401 });
 
-	await Promise.all([
-		db.insert(roles).values({
-			userId: user.id,
-			projectId: Number(projectId),
-		}),
-		kv.del(`invite:${code}`),
-	]);
+    await Promise.all([
+        db.insert(roles).values({
+            userId: user.id,
+            projectId: Number(projectId),
+        }),
+        kv.del(`invite:${code}`),
+    ]);
 
-	after(async () => {
-		const freshUser = await getUserFromEmail(email);
-		const stream = createWebsocketStream();
+    after(async () => {
+        const freshUser = await getUserFromEmail(email);
+        const stream = await createWebsocketStream();
 
-		freshUser.roles
-			.find((role) => role.projectId === Number(projectId))
-			?.project.roles.forEach((role) => {
-				stream.send(
-					String(role.userId),
-					"project-member-added",
-					JSON.stringify({
-						projectId,
-						userId: role.userId,
-						user,
-					}),
-				);
-			});
-	});
+        freshUser.roles
+            .find((role) => role.projectId === Number(projectId))
+            ?.project.roles.forEach((role) => {
+                stream.send(
+                    String(role.userId),
+                    "project-member-added",
+                    JSON.stringify({
+                        projectId,
+                        userId: role.userId,
+                        user,
+                    })
+                );
+            });
+    });
 
-	return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
 }
